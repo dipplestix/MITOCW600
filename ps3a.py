ffe01c51b9553ec9b7e9239750eb4fe4abcd2932
unknown <chrism315@gmail.com>
2015-01-04 14:24:32 -0500

    Updated problem 3

--- ps3a.py
+++ ps3a.py
@@ -12,15 +12,3 @@ def countSubStringMatchRecursive(string, target, i=0):
         return 1 + countSubStringMatchRecursive(string[index+1:], target)
     else:
         return 0
-    
-assert countSubStringMatch('aaa', 'b') == 0
-assert countSubStringMatch('aaa', 'aa') == 2
-assert countSubStringMatch('abba', 'ba') == 1
-assert countSubStringMatch('abbaabba', 'ba') == 2
-assert countSubStringMatch('abababab', 'aba') == 3
-
-assert countSubStringMatchRecursive('aaa', 'b') == 0
-assert countSubStringMatchRecursive('aaa', 'aa') == 2
-assert countSubStringMatchRecursive('abba', 'ba') == 1
-assert countSubStringMatchRecursive('abbaabba', 'ba') == 2
-assert countSubStringMatchRecursive('abababab', 'aba') == 3
