e8fa735d7d36157134b37f199206f429b0a29c63
unknown <chrism315@gmail.com>
2015-01-04 14:20:47 -0500

    Updated problem 4

--- ps4.py
+++ ps4.py
@@ -17,3 +17,41 @@ def nestEggVariable(salary, save, growthRates):
             loopAmount = amount[-1]*(1+rate/100) + salary*save/100
             amount.append(loopAmount)
     return amount
+    
+def postRetirement(savings, growthRates, expenses):
+    balance = []
+    for rate in growthRates:
+        if len(balance) == 0:
+            balance.append(savings*(1+rate/100) - expenses)
+        else:
+            newbalance = balance[-1]*(1+rate/100) - expenses
+            balance.append(newbalance)
+    return balance
+    
+def findMaxExpenses(salary, save, preRetireGrowthRates, postRetireGrowthRates,
+                    epsilon):
+    savings = nestEggVariable(salary, save, preRetireGrowthRates)[-1]
+    low = 0
+    high = savings
+    guess = (low + high)/2
+    balance = postRetirement(savings, postRetireGrowthRates, guess)
+    while abs(balance[-1]) > epsilon:
+            if balance[-1] > 0:
+                low = guess
+            else:
+                high = guess
+            guess = (low + high)/2
+            balance = postRetirement(savings, postRetireGrowthRates, guess)
+    return guess
+
+def testFindMaxExpenses():
+    salary                = 10000
+    save                  = 10
+    preRetireGrowthRates  = [3, 4, 5, 0, 3]
+    postRetireGrowthRates = [10, 5, 0, 5, 1]
+    epsilon               = .01
+    expenses = findMaxExpenses(salary, save, preRetireGrowthRates,
+                               postRetireGrowthRates, epsilon)
+    print(expenses)
+    # Output should have a value close to:
+    # 1229.95548986
